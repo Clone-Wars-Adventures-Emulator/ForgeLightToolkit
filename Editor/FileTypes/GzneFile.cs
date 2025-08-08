40f14ae347f3c499ce515fe075a808aff5850cdd
Joseph Terribile <kartingman25@gmail.com>
2025-08-08 02:13:15 -0400

    Added support for InvisibleWall parsing and generation

--- Editor/FileTypes/GzneFile.cs
+++ Editor/FileTypes/GzneFile.cs
@@ -1,15 +1,11 @@
+using ForgeLightToolkit.Editor;
 using System;
 using System.IO;
 using System.Collections.Generic;
-
 using UnityEngine;
 
-using ForgeLightToolkit.Editor;
-
-namespace ForgeLightToolkit
-{
-    public class GzneFile : ScriptableObject
-    {
+namespace ForgeLightToolkit {
+    public class GzneFile : ScriptableObject {
         public bool HideTerrain;
 
         public int ChunkSize;
@@ -27,33 +23,32 @@ namespace ForgeLightToolkit
         public int Unknown9;
         public int Unknown10;
 
-        [HideInInspector]
         public List<EcoData> EcoData = new();
 
-        [HideInInspector]
         public List<FloraDefinition> FloraDefinitions = new();
 
-        [HideInInspector]
-        public List<List<Vector3>> InvisibleWalls = new();
+        public List<Mesh> wallMeshes = new();
 
-        public bool Load(string filePath)
-        {
+        public bool Load(string filePath) {
             name = Path.GetFileNameWithoutExtension(filePath);
 
             var reader = new Reader(File.OpenRead(filePath));
 
             var magic = new string(reader.ReadChars(4));
 
-            if (magic != "GZNE")
+            if (magic != "GZNE") {
                 return false;
+            }
 
             var version = reader.ReadInt32();
 
-            if (version > 3)
+            if (version > 3) {
                 throw new NotSupportedException($"Cannot process file with version {version}");
+            }
 
-            if(version >= 3)
+            if (version >= 3) {
                 HideTerrain = (reader.ReadInt32() & 1) == 1;
+            }
 
             ChunkSize = reader.ReadInt32();
             TileSize = reader.ReadInt32();
@@ -68,8 +63,7 @@ namespace ForgeLightToolkit
 
             var ecoDataCount = reader.ReadInt32();
 
-            for (var i = 0; i < ecoDataCount; i++)
-            {
+            for (var i = 0; i < ecoDataCount; i++) {
                 var ecoData = new EcoData();
 
                 ecoData.Deserialize(reader);
@@ -79,8 +73,7 @@ namespace ForgeLightToolkit
 
             var floraDefinitionCount = reader.ReadInt32();
 
-            for (var i = 0; i < floraDefinitionCount; i++)
-            {
+            for (var i = 0; i < floraDefinitionCount; i++) {
                 var floraDefinition = new FloraDefinition();
 
                 floraDefinition.Deserialize(reader);
@@ -88,23 +81,51 @@ namespace ForgeLightToolkit
                 FloraDefinitions.Add(floraDefinition);
             }
 
-            if (version >= 2)
-            {
+            if (version >= 2) {
                 var invisibleWallCount = reader.ReadInt32();
 
-                if (invisibleWallCount > 10000)
+                if (invisibleWallCount > 10000) {
                     throw new IndexOutOfRangeException($"Invalid number of Invisible Walls: {invisibleWallCount}");
+                }
 
-                for (var i = 0; i < invisibleWallCount; i++)
-                {
+                for (var i = 0; i < invisibleWallCount; i++) {
                     var invisibleWallVertexCount = reader.ReadInt32();
 
                     var invisibleWallVertices = new List<Vector3>();
-
-                    for (var j = 0; j < invisibleWallVertexCount; j++)
+                    for (var j = 0; j < invisibleWallVertexCount; j++) {
                         invisibleWallVertices.Add(reader.ReadVector3());
+                    }
 
-                    InvisibleWalls.Add(invisibleWallVertices);
+                    // unfortunately, ForgeLight saved these walls a TriangleStrip meshes (see https://en.wikipedia.org/wiki/Triangle_strip)
+                    // and unity our beloved doesnt have a way to generate a mesh form this format out of the box, so we have to deconstruct that outselves
+                    // given the hypothetical triangle strip
+                    // 0---2---4
+                    // |  /|  /|
+                    // | / | / |
+                    // |/  |/  |
+                    // 1---3---5
+                    // the indexes in unity's format would be (0, 1, 2), (2, 1, 3), (2, 3, 4), (4, 3, 5).
+                    // This can be generalized to a strip with [3,n] verticies, where the faces are constructed by looping through the verticies starting at
+                    // index 2 (the third vertex) and alternating between the faces (idx - 2, idx - 1, idx) and (idx - 1, idx - 2, idx)
+                    var idxs = new List<int>();
+                    for (int idx = 2; idx < invisibleWallVertices.Count; idx++) {
+                        if (idx % 2 == 0) {
+                            idxs.Add(idx - 2);
+                            idxs.Add(idx - 1);
+                            idxs.Add(idx);
+                        } else {
+                            idxs.Add(idx - 1);
+                            idxs.Add(idx - 2);
+                            idxs.Add(idx);
+                        }
+                    }
+
+                    var mesh = new Mesh {
+                        name = $"{name}_InvisibleWall_{i}"
+                    };
+                    mesh.SetVertices(invisibleWallVertices);
+                    mesh.SetIndices(idxs, MeshTopology.Triangles, 0);
+                    wallMeshes.Add(mesh);
                 }
             }
 
--- Editor/Importers/GzneImporter.cs
+++ Editor/Importers/GzneImporter.cs
@@ -1,31 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEditor.AssetImporters;
 
-using ForgeLightToolkit.Editor.FileTypes;
-
-namespace ForgeLightToolkit.Editor.Importers
-{
+namespace ForgeLightToolkit.Editor.Importers {
     [ScriptedImporter(1, "gzne")]
-    public class GzneImporter : ScriptedImporter
-    {
-        public override void OnImportAsset(AssetImportContext ctx)
-        {
-            if (string.IsNullOrEmpty(ctx.assetPath))
-            {
+    public class GzneImporter : ScriptedImporter {
+        public override void OnImportAsset(AssetImportContext ctx) {
+            if (string.IsNullOrEmpty(ctx.assetPath)) {
                 ctx.LogImportError($"Invalid asset path. ({ctx.assetPath})");
                 return;
             }
 
             var gzneFile = ScriptableObject.CreateInstance<GzneFile>();
 
-            if (!gzneFile.Load(ctx.assetPath))
-            {
+            if (!gzneFile.Load(ctx.assetPath)) {
                 ctx.LogImportError($"Failed to load gzne file. ({ctx.assetPath})");
                 return;
             }
 
             ctx.AddObjectToAsset("Gzne", gzneFile);
             ctx.SetMainObject(gzneFile);
+            for (int i = 0; i < gzneFile.wallMeshes.Count; i++) {
+                var mesh = gzneFile.wallMeshes[i];
+                ctx.AddObjectToAsset($"InvisibleWall{i}", mesh);
+            }
         }
     }
 }
